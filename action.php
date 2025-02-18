efdaac29f494db6bd0160a8d13395c9e4a0885bc
farhantaskinismail <128569990+farhantaskinismail@users.noreply.github.com>
2025-02-19 01:07:40 +0600

    aaaaaaaa

--- action.php
+++ action.php
@@ -1,15 +1,1 @@
-<?php
-$Email = $_POST['email'];  
-$Passwordd = $_POST['pwd'];
-$conn = new mysqli('localhost','root','','for_facebook');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
-}else{
-    $stmt = $conn->prepare("insert into public(email, password)values(?,?)");
-    $stmt->bind_param("ss",$Email, $Passwordd);
-    $stmt->execute();
-    echo "registration hoisa...";
-    $stmt->close();
-    $conn->close();
-}
-?>
+
